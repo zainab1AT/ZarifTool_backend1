57e11abda8de3f86708dc51a1b03b7050d730145
Tala-Al-Dibs <talad6791@gmail.com>
2025-01-30 20:30:04 +0200

    Initial commit

---
+++ src/main/java/com/project/physio_backend/PhysioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.physio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysioBackendApplication.class, args);
+	}
+
+}
