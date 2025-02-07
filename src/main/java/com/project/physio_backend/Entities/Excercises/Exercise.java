e80543c4ef8236c25feaf9be3713fd99e0b41c86
Tala-Al-Dibs <talad6791@gmail.com>
2025-02-07 21:26:41 +0200

    starting on making Exercises and Physiotherapy Entities

--- src/main/java/com/project/physio_backend/Entities/Excercises/Exercise.java
+++ src/main/java/com/project/physio_backend/Entities/Excercises/Exercise.java
@@ -1,5 +1,22 @@
 package com.project.physio_backend.Entities.Excercises;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "exercise")
+@Data
 public class Exercise {
-    
+
+    @Column(name = "exercise_ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long exerciseID;
+
+    private String exerciseImageURI;
+
+    private String exerciseDescription;
+
+    private int exerciseLength;
+
 }
--- src/main/java/com/project/physio_backend/Entities/Physiotherapists/Physiotherapist.java
+++ src/main/java/com/project/physio_backend/Entities/Physiotherapists/Physiotherapist.java
@@ -1,5 +1,30 @@
 package com.project.physio_backend.Entities.Physiotherapists;
 
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "physiotherapist")
+@Data
 public class Physiotherapist {
-    
+
+    @Column(name = "physiotherapist_ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long physiotherapistID;
+
+    private String clicncName;
+
+    private long phonenumber;
+
+    private double price;
+
+    private String address;
+
+    private List<String> workingDays;
+
+    @OneToMany(mappedBy = "physiotherapist", cascade = CascadeType.ALL)
+    private List<WorkingHours> workingHours;
 }
