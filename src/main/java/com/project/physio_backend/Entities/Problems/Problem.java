cca3f73d90a62f2f75fc037358b05c56ed1c4623
NourAlhuda03 <145063949+NourAlhuda03@users.noreply.github.com>
2025-02-08 16:45:21 +0200

    added progress,problem and report entities

--- src/main/java/com/project/physio_backend/Entities/Problems/Problem.java
+++ src/main/java/com/project/physio_backend/Entities/Problems/Problem.java
@@ -1,5 +1,50 @@
 package com.project.physio_backend.Entities.Problems;
 
+import java.util.ArrayList;
+
+import com.project.physio_backend.Entities.Excercises.Exercise;
+import com.project.physio_backend.Entities.Reports.Report;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "problem")
 public class Problem {
-    
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "Problem_id", nullable = false)
+  private Long problemID;
+
+  @Column(name = "descriptive_image")
+  private String descriptiveImage;
+
+  @Column(columnDefinition = "TEXT")
+  private String description;
+
+  @ManyToMany
+  @JoinTable(name = "problem_exercise", joinColumns = @JoinColumn(name = "problem_id"), inverseJoinColumns = @JoinColumn(name = "exercise_id"))
+  private List<Exercise> exercises = new ArrayList<>();
+
+  @ManyToMany(mappedBy = "problems")
+  private List<Report> reports = new ArrayList<>();
+
+  public Problem() {
+  }
+
+  public Problem(String descriptiveImage, String description, List<Exercise> exercises) {
+    this.descriptiveImage = descriptiveImage;
+    this.description = description;
+    this.exercises = exercises;
+  }
+
+  public Problem(String descriptiveImage, String description) {
+    this.descriptiveImage = descriptiveImage;
+    this.description = description;
+  }
+
 }
--- src/main/java/com/project/physio_backend/Entities/Progress/Progress.java
+++ src/main/java/com/project/physio_backend/Entities/Progress/Progress.java
@@ -1,5 +1,35 @@
 package com.project.physio_backend.Entities.Progress;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.project.physio_backend.Entities.Excercises.Exercise;
+
+@Data
+@Entity
+@Table(name = "progress")
 public class Progress {
-    
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "progress_id", nullable = false)
+  private Long progressID;
+
+  private LocalDateTime timestamp;
+
+  @OneToMany(mappedBy = "progress", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Exercise> exercises = new ArrayList<>();
+
+  public Progress() {
+  }
+
+  public Progress(LocalDateTime timestamp, List<Exercise> exercises) {
+    this.timestamp = timestamp;
+    this.exercises = exercises;
+  }
+
 }
